2223ffe07c570f78b287635a2bc8730335cdd326
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-04-13 15:05:44 +0000

    Automatically backed up by Learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,10 +1,3 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
 
-it 'has a summary' do
-  post = Post.create!(title: "My title", description: "The post description")
-  expect(post.post_summary).to eq("My title - The post description")
-end
