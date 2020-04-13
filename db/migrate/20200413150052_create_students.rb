7eaa6c23259f99a2c31b626c8ac6cf77aba36ee6
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-04-13 15:01:26 +0000

    Automatically backed up by Learn

--- db/migrate/20200413150052_create_students.rb
+++ db/migrate/20200413150052_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     create_table :students do |t|
       t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
